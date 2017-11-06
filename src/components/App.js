31ed61f89dd4c97b10048ecb800f3db74ccf81ea
chris martinez <chris martinez>
2017-11-06 13:47:28 -0800

    finish tut

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Header from './Header'
+import Home from './Home'
+import About from './About'
+import Courses from './Courses'
+import Teachers from './Teachers'
+import NotFound from './NotFound'
+
+import Featured from './Featured'
+
 
 const App = () => (
-  <div className="container">
+  <BrowserRouter>
+    <div className="container">
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" render={()=> <About title='Boom'/>   }/>
+        <Route path="/courses" component={Courses}/>
+        <Route exact path="/teachers" component={Teachers}/>
+        <Route path="/teachers/:topic/:name" component={Featured}/>
 
-  </div>
+        <Route component={NotFound}/>
+      </Switch>
+    </div>
+
+  </BrowserRouter>
 );
 
 export default App;
